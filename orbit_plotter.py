37111451488f069d9c61cf82e75d6ca93a8b239d
strumber <stormurbjorn24@gmail.com>
2022-10-16 18:06:14 +0200

    Asteroid info updated

--- orbit_plotter.py
+++ orbit_plotter.py
@@ -23,7 +23,6 @@ DROPBOX_ACCESS_TOKEN = os.getenv('DROPBOX_ACCESS_TOKEN')
 
 
 def dropbox_connect():
-    """Create a connection to Dropbox."""
 
     try:
         dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
@@ -33,19 +32,6 @@ def dropbox_connect():
 
 
 def dropbox_upload_file(local_path, local_file, dropbox_file_path):
-    """Upload a file from the local machine to a path in the Dropbox app directory.
-
-    Args:
-        local_path (str): The path to the local file.
-        local_file (str): The name of the local file.
-        dropbox_file_path (str): The path to the file in the Dropbox app directory.
-
-    Example:
-        dropbox_upload_file('.', 'test.csv', '/stuff/test.csv')
-
-    Returns:
-        meta: The Dropbox file metadata.
-    """
 
     try:
         dbx = dropbox_connect()
